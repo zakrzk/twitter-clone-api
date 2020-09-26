e78786f81b3a720961e187d31961a92cc315859c
zakrzk <bzakrzewski1@gmail.com>
2020-09-26 18:45:58 +0100

    move DB vars to .env

--- lib/util/database.ts
+++ lib/util/database.ts
@@ -2,7 +2,10 @@ import * as mysql from "mysql2";
 import * as Sequelize from 'sequelize';
 
 // @ts-ignore
-export const sequelize  = new Sequelize('twitter', 'root', 'root', {
-    dialect: 'mysql',
-    host: 'localhost'
-});
+export const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD, {
+        dialect: 'mysql',
+        host: process.env.DB_HOST
+    });
